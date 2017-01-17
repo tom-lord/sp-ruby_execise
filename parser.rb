f1b54a3abe656ccafea147fcc43d6fa862fb3c15
Tom Lord <tom.lord@comlaude.com>
2017-01-17 00:44:58 +0000

    (Extension) Commented out usage example of RandomFormatter

--- parser.rb
+++ parser.rb
@@ -8,3 +8,10 @@ puts parser.display_page_views(TotalDisplayFormatter.new)
 puts
 puts "Unique page views:"
 puts parser.display_page_views(UniqueDisplayFormatter.new)
+
+
+# Technically this is not part of the solution,
+# so I've left it commented out... Feel free to uncomment.
+#puts
+#puts "Random page views:"
+#puts parser.display_page_views(RandomDisplayFormatter.new)
