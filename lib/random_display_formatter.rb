4df955fff19b683e60b65410c38b90de06b74bf6
Tom Lord <tom.lord@comlaude.com>
2017-01-17 00:42:14 +0000

    (Extension) Defined a RANDOM formatter, which can be used in place
    This will select a random IP that visited each address, and display
    lines in a random order.
    
    I could have used SecureRand for true randomness, but w/e.

---
+++ lib/random_display_formatter.rb
@@ -0,0 +1,13 @@
+class RandomDisplayFormatter < BaseDisplayFormatter
+  def metric(views)
+    views.sample
+  end
+
+  def sort_metric(views)
+    rand
+  end
+
+  def description
+    'is a random IP that visited here'
+  end
+end
